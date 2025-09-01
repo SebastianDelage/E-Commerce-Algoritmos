46a976252886a9ad3ba0d5f47eef9615ad276441
matippe <mateopp77@hotmail.com>
2025-09-01 08:23:35 -0300

    1er Commit

---
+++ E-commerce/E-commerce/src/Componentes/Header.jsx
@@ -0,0 +1,46 @@
+import "./Header.css";
+
+const Header = () => {
+
+    return(
+
+        <div className="container py-2">
+            <div className="row align-items-center">
+                <div className="col-2">
+                <img src="" alt="Imagen" className="img-fluid" />
+                </div>
+
+                <div className="col text-center">
+                    <h1 className="m-0 display-1">E-COMMERCE</h1>
+                </div>                
+
+                <div className="col-2 d-flex justify-content-end gap-3">
+                    {/* COMO QUE ESTO ES UN COMENTARIO*/}
+
+                    {/* Parece una boludez pero despues dentro de cada div va a redirigirte a otra vista, por eso esta hecho asi */}
+                    <div className="ruta">
+                        <i className="bi bi-cart"/>
+                    </div>
+                    <div className="ruta">
+                        <i className="bi bi-heart-fill"/>
+                    </div>            
+                    <div className="ruta">
+                        <i className="bi bi-search"/>
+                    </div>                                        
+                </div>
+            </div>
+
+            {/* Fila inferior: menú */}
+            <div className="row mt-2">
+                <div className="col d-flex justify-content-around">
+                    <a href="#">Hombres</a>
+                    <a href="#">Mujeres</a>
+                    <a href="#">Promociones</a>
+                    <a href="#">Carrito</a>
+                </div>
+            </div>
+            </div>
+    );
+}
+
+export default Header;
