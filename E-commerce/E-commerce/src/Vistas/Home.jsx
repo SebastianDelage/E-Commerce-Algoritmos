be3067a099e33178b911ae1a991ab44c8ad4cb6c
Mateo Prestia <prestia.mateo@gmail.com>
2025-09-25 09:50:52 -0300

    Agregado de imagenes de muestra

--- E-commerce/E-commerce/src/Vistas/Home.jsx
+++ E-commerce/E-commerce/src/Vistas/Home.jsx
@@ -12,16 +12,16 @@ export default function Home() {
                 </div>
                 <div class="wsk-cp-text">
                   <div class="category">
-                    <span>Ethnic</span>
+                    <span>T-Shirt</span>
                   </div>
                   <div class="title-product">
-                    <h3>My face not my heart</h3>
+                    <h3>Remera Algodon BoxyFit Hype </h3>
                   </div>
                   <div class="description-prod">
-                    <p>Description Product tell me how to change playlist height size like 600px in html5 player. player good work now check this link</p>
+                    <p>Remerita</p>
                   </div>
                   <div class="card-footer">
-                    <div class="wcf-left"><span class="price">Rp500.000</span></div>
+                    <div class="wcf-left"><span class="price">$ 150.000</span></div>
                     <div class="wcf-right"><a href="#" class="buy-btn"><i class="zmdi zmdi-shopping-basket"></i></a></div>
                   </div>
                 </div>
