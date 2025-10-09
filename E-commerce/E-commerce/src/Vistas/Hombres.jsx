6b2285f374be2359b85c77bd8c4b200401728550
Hilet Aulas <hilet@HILET.LOCAL>
2025-10-09 09:38:32 -0300

    Diseño de card para añadir producto

--- E-commerce/E-commerce/src/Vistas/Hombres.jsx
+++ E-commerce/E-commerce/src/Vistas/Hombres.jsx
@@ -1,4 +1,29 @@
+import "../assets/styles/ProductItem.css";
+
 export default function Hombres() {
-    return <h1>Página de hombres</h1>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col productCardAddCol">
+            <div className="row">
+              <div className="mb-3">
+                <div className="productCardAdd align-content-center">
+                  <div className="squarePlus">
+                    <p className="productCard-text"><i className="bi bi-plus-lg"></i></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <div className="productCard-subtitle">
+                  <p className="productCard-text">Cargar Producto</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
   
